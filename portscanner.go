195b375123c677c34f06a8d8fd47d527dd63c9cd
Peter Sjolin <pigfoxeria@gmail.com>
2025-03-27 17:43:18 -0700

    wip

--- portscanner.go
+++ portscanner.go
@@ -224,7 +224,7 @@ func parsePorts(portStr string) []int {
 
 func main() {
 	fmt.Println("Starting port scanner")
-	//os.Exit(0)
+	os.Exit(0)
 	//defer recoverPanic()
 	go update()
 	startIP := flag.String("start", "192.168.1.1", "Starting IP address")
