08eaa590852b193fc5e3118cf5da323046178b67
Peter Sjolin <pigfoxeria@gmail.com>
2025-03-28 14:06:20 -0700

    WIP

--- tests.sh
+++ tests.sh
@@ -3,5 +3,7 @@ set -e
 set -x       
 clear   
 
-go test -v -cover -cpuprofile=cpu.prof -memprofile=mem.prof -bench=.
-
+go test -v -cover -cpuprofile=cpu.prof -memprofile=mem.prof -bench=. -benchmem
+ls -lh mem.prof
+ls -lh cpu.prof
+go tool pprof -alloc_space -top mem.prof
