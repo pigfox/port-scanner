2964b487a69bafb5fc25b3100417e57d77e1cd20
Peter Sjolin <pigfoxeria@gmail.com>
2025-03-28 16:29:42 -0700

    changed error behaviour

--- helpers.go
+++ helpers.go
@@ -12,7 +12,7 @@ import (
 func init() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("Error loading .env file")
 	}
 
 	brevoUrl := os.Getenv("BREVO_URL")
